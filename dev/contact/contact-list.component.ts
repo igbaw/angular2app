64a2d0e0e488ae6b8650cf4e4878d37dd568c949
igbaw <igb.adi@gmail.com>
2016-01-27 07:36:33 +0800

    additional commit

--- dev/contact/contact-list.component.ts
+++ dev/contact/contact-list.component.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
 import {ContactComponent} from './contact.component';
+import {ContactService} from './contact.service';
+import {Contact} from './contact';
 
 @Component({
     selector: 'contact-list',
@@ -14,41 +16,22 @@ import {ContactComponent} from './contact.component';
                 <contact [contact]="selectedContact"></contact>
                `,
      directives:[ContactComponent],
+     providers: [ContactService],
      styleUrls: ["../src/css/app.css"]
     
 })
 
 export class ContactListComponent{
-      public contacts = [{firstName: "Adi",
-                      lastName: "Wibawa",
-                      phone: "234234",
-                      email: "igb.adi@gmail.com"    
-                      },
-                      {firstName: "sd",
-                      lastName: "sdsad",
-                      phone: "234234234",
-                      email: "igb.asd@gmail.com"    
-                      },
-                      {firstName: "A123123di",
-                      lastName: "asdasd",
-                      phone: "235324",
-                      email: "asd.adi@gmail.com"    
-                      },
-                      {firstName: "gasdsa",
-                      lastName: "fasdas",
-                      phone: "234234",
-                      email: "igfdfb.adi@gmail.com"    
-                      },
-                      {firstName: "asfaf",
-                      lastName: "asdasd",
-                      phone: "234234234",
-                      email: "igfggb.adi@gmail.com"    
-                      }
-                      ];
+     public contacts: Contact[];
      public selectedContact = {};
+     private contactService;
+     
+     constructor(contactService : ContactService){}
      
      onSelect(contact){
          this.selectedContact = contact;
      }
+     
+     
     
 }
